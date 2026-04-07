28a2f2fbe37462e62102802e1867b40e0553a12d
tasktogether <tasktogethercontact@gmail.com>
2026-04-07 01:53:06 -0400

    Update AuthContext.tsx

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -276,8 +276,13 @@ const updateApplicationStatus = async (
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
+
   if (context === undefined) {
+    console.error("useAuth called outside AuthProvider");
     throw new Error('useAuth must be used within an AuthProvider');
   }
+
+  console.log("Auth context loaded:", context);
+
   return context;
 };
