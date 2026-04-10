7977d2aa95ab5f6345bda86c84fff50bfd01c019
tasktogether <tasktogethercontact@gmail.com>
2026-04-09 23:17:15 -0400

    Update LoginPage.tsx

--- src/app/pages/LoginPage.tsx
+++ src/app/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, Link } from 'react-router';
+import { useNavigate, Link } from 'react-router-dom';
 import { Heart, Eye, EyeOff, LogIn, AlertCircle } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
 
