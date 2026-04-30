e59a5c27334f27f8d9ca5cea0df71978be7cd162
tasktogether <tasktogethercontact@gmail.com>
2026-04-30 12:44:08 -0700

    Update DirectorSettings.tsx

--- src/app/pages/DirectorSettings.tsx
+++ src/app/pages/DirectorSettings.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../../lib/supabase.ts';
 
 export default function DirectorSettings() {
   const [loading, setLoading] = useState(true);
