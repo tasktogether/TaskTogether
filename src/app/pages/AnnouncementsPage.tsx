30842ff4407985358760be02494fadd0edb15e86
tasktogether <tasktogethercontact@gmail.com>
2026-05-01 19:23:40 -0700

    Update AnnouncementsPage.tsx

--- src/app/pages/AnnouncementsPage.tsx
+++ src/app/pages/AnnouncementsPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../../lib/supabase.ts';
+import { supabase } from '../../../lib/supabase';
 import { useNavigate } from 'react-router-dom';
 
 export default function AnnouncementsPage() {
